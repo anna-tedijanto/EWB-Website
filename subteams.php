92743a52525bfcd5be166a25ecb1ec0e94c4e08e
Jake Carnavalla <jakecarnavalla@Jakes-MacBook-Pro.local>
2017-05-02 15:43:08 -0400

    added login

--- subteams.php
+++ subteams.php
@@ -49,15 +49,16 @@
     <div id = "content">        
         <div class="container">
           <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 align="center" wider">
-              <ul class="nav nav-tabs">
-                <li><a data-toggle="tab" href="#exec">Executive Board</a></li>
-                <li><a data-toggle="tab" href="#bus-des">Business/Design</a></li>
-                <li><a data-toggle="tab" href="#dom">Domestic</a></li>
-                <li><a data-toggle="tab" href="#calcha">Calcha Close-Out</a></li>
-                <li><a data-toggle="tab" href="#cs">Computer Science</a></li>
-                <li><a data-toggle="tab" href="#imp">Implementation</a></li>
-              </ul>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 align="center" wider">
+                <div class="subteam-bar">
+                  <ul class="nav nav-tabs">
+                    <li><a data-toggle="tab" href="#exec">Executive Board</a></li>
+                    <li><a data-toggle="tab" href="#bus-des">Business/Design</a></li>
+                    <li><a data-toggle="tab" href="#dom">Domestic</a></li>
+                    <li><a data-toggle="tab" href="#calcha">Calcha Close-Out</a></li>
+                    <li><a data-toggle="tab" href="#imp">Implementation</a></li>
+                  </ul>
+                </div>
             </div>
           </div>
               <div class="tab-content">
@@ -151,35 +152,6 @@
                 <div id="calcha" class="tab-pane fade">
                 <div class="row">
                     <h1 class="wider grey"><strong>Calcha Close-Out</strong></h1>
-                  </div><!--end row div-->
-                  <div class="row">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-5 wider">
-                        
-                            <p class="justify">Engineers Without Borders-Cornell is a group of passionate and highly
-                            motivated students who aim to create long-lasting solutions to problems in
-                            international communities. Members are given the opportunity to nurture their
-                            creative and technical expertise by designing and implementing sustainable
-                            engineering projects. Through collaboration with communities in developing
-                            countries around the world, our members are exposed to diverse cultures
-                            while gaining academic experience outside of the classroom environment.
-                            </p>
-                            <p class="justify">Our growing team of 52 dedicated students includes undergraduate and
-                            graduate students from several of Cornell’s colleges including the College of
-                            Engineering, the College of Arts and Sciences, and the College of Agriculture
-                            and Life Sciences. Our network also extends beyond Cornell’s campus to over
-                            250 EWB chapters.
-                            </p>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-7 wider">
-                        <img class="img-fluid" src="images/team.jpg" alt="team">
-                        <img class="img-fluid" src="images/pie_chart.png" alt="pie">
-                    </div>
-                </div><!--end row div-->
-                </div><!--end tab-pane div-->
-
-                <div id="cs" class="tab-pane fade">
-                <div class="row">
-                    <h1 class="wider grey"><strong>Computer Science</strong></h1>
                   </div><!--end row div-->
                   <div class="row">
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-5 wider">
