23fe4b5970aa0d5ace4ff8c731e473ae3f56035c
Justin Peterson <Justin@Justins-MacBook-Pro-2.local>
2017-05-15 18:54:26 -0400

    updated margins and subteam page

--- subteams.php
+++ subteams.php
@@ -90,27 +90,32 @@
                   </div><!--end row div-->
                   <div class="row">
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-5 wider">
-                        
-                            <p class="justify">Engineers Without Borders-Cornell is a group of passionate and highly
-                            motivated students who aim to create long-lasting solutions to problems in
-                            international communities. Members are given the opportunity to nurture their
-                            creative and technical expertise by designing and implementing sustainable
-                            engineering projects. Through collaboration with communities in developing
-                            countries around the world, our members are exposed to diverse cultures
-                            while gaining academic experience outside of the classroom environment.
-                            </p>
-                            <p class="justify">Our growing team of 52 dedicated students includes undergraduate and
-                            graduate students from several of Cornell’s colleges including the College of
-                            Engineering, the College of Arts and Sciences, and the College of Agriculture
-                            and Life Sciences. Our network also extends beyond Cornell’s campus to over
-                            250 EWB chapters.
+                            <p class="justify">The Business and Design Team focus on financing EWB's projects by running
+                                events and raising awareness about the organization on Cornell's campus and in the 
+                                Ithaca area.
+                                On the business side, students conduct several fundraisers and campaigns throughout the school year; 
+                                such as the Charity Concert, fundraising campaigns, an annual Skate Night, and several sales ranging from bakesales to 
+                                finals week care package sales. Additionally the business team maintains relations with our corporate sponsors, donors, 
+                                and alumni to keep them informed about and involved with the team.
                             </p>
                     </div>
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-7 wider">
                         <img class="img-fluid" src="images/team.jpg" alt="team">
-                        <img class="img-fluid" src="images/pie_chart.png" alt="pie">
                     </div>
-                </div><!--end row div-->
+                  </div><!--end row div-->
+                  <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 wider">
+                        <img class="img-fluid" src="images/team.jpg" alt="team">
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 wider">
+                            <p class="justify">The design side of the team concentrates on branding by developing 
+                                and managing all of the team's promotional material. This includes designing all the 
+                                advertising material for events and maintaining an online prescence through our website and social media pages. 
+                                Students on this interdisciplinary subteam gain skills in event-management, directing, 
+                                online design, and are essential to making our projects a reality.
+                            </p>
+                    </div>
+                  </div><!--end row div-->
                 </div><!--end tab-pane div-->
 
                 <div id="cr" class="tab-pane fade">
